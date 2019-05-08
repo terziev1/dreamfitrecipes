1a9faab9ea72f37564375b770fb52b9cead0df6f
Kostadin Terziev <kostadinterziev@Kostadins-MacBook-Pro.local>
2019-05-08 14:01:56 +0300

    test

--- nuxt.config.js
+++ nuxt.config.js
@@ -94,7 +94,7 @@ const config = {
           // map entries to URLs
           ...entries.items.map(entry => `/blog/${entry.fields.slug}`),
           // map all possible tags to URLs
-          // ...postType.fields.find(field => field.id === 'tags').items.validations[0].in.map(tag => `/tags/${tag}`)
+          ...postType.fields.find(field => field.id === 'tags').items.validations[0].map(tag => `/tags/${tag}`)
         ]
       })
     }
