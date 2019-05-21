b5d335d0bc224520124e99eb769a273150322e70
Kostadin Terziev <kostadinterziev@Kostadins-MacBook-Pro.local>
2019-05-21 19:45:31 +0300

    testing image fix

--- components/article-preview-big.vue
+++ components/article-preview-big.vue
@@ -2,7 +2,7 @@
   <article>
     <nuxt-link class="thumbnail-wrapper" :to="{ name: 'blog-slug', params: { slug: post.fields.slug }}">
     <img class="thumbnail"
-      :src="post.fields.heroImage.fields.file.url"
+      :src="`${post.fields.heroImage.fields.file.url}?q=80`"
 
     >
     </nuxt-link>
